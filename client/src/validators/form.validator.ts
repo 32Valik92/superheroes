8cb9e87b73fd8bf79f5e438c34787806f2e8e91f
32Valik92 <valokstrukalo@gmail.com>
2023-09-19 00:00:59 +0300

    validator fix

--- client/src/validators/form.validator.ts
+++ client/src/validators/form.validator.ts
@@ -1,10 +1,10 @@
 import Joi from "joi";
 
 export const formValidator = Joi.object({
-    nickname: Joi.string().regex(/^[a-zA-Z0-9]{1,12}$/).required().messages({
-        'string.pattern.base': 'nickname: You can use only Latin letters and it is required and length <= 12'
+    nickname: Joi.string().regex(/^[a-zA-Z0-9]{1,16}$/).required().messages({
+        'string.pattern.base': 'nickname: Only Latin letters without space and it is required and length <= 16'
     }),
-    real_name: Joi.string().regex(/^[A-Za-z]+$/).required().messages({
+    real_name: Joi.string().regex(/^[A-Za-z ]+$/).required().messages({
         'string.pattern.base': 'real_name: You can use only Latin letters and it is required'
     }),
     origin_description: Joi.string().required(),
